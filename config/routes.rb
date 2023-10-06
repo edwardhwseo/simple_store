43878a5d9a671666788a9a864aedd5443d5453cc
eseo <edwardhwseo@gmail.com>
2023-10-06 08:42:02 -0500

    Update products_controller.rb
    Added Products Controller show action/view and linked each product name to its respective show in index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #get 'products/index'
+  #resources :products, only: %i[index show]
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
